6c01fbde0731280f8609a8c429bff7e4feef71bc
yalmea <yalmea@boldt.com.ar>
2022-08-08 13:51:45 -0300

    cambios en configuracion

--- eureka-service/src/main/java/com/tutorial/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/tutorial/eurekaservice/EurekaServiceApplication.java
@@ -1,14 +1,21 @@
 package com.tutorial.eurekaservice;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServiceApplication {
+	private static final Logger log = LogManager.getLogger(EurekaServiceApplication.class.getName());
 
 	public static void main(String[] args) {
+		log.info("Inicando aplicacion");
+		log.error("Inicando aplicacion");
+		log.debug("Inicando aplicacion");
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
